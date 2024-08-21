327f84438e5778a7b979589b08d2740c053fcd02
VegaMartins <vgmartinsgomes@aluno.uespi.br>
2024-08-20 21:53:42 -0300

    Adição do JavaScript

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const botaoMostrarProjetos = (document.querySelector('.btn-mostrar-projetos'));
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+console.log(projetosInativos);
+
+//botaoMostrarProjetos.addEventListener('click', () => {
+
+//projetosInativos.forEach(projetoInativo => {
+// projetoInativo.classList.add('ativo');
+//});
+
+//botaoMostrarProjetos.classList.add("remover");
+
+
+//})
